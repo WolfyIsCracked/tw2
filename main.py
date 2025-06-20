4a279a3c346375c919294c185f33bc4d2b8dd261
WolfyIsCracked <102771771+WolfyIsCracked@users.noreply.github.com>
2025-06-20 14:33:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ twitch_miner = TwitchChannelPointsMiner(
 
 twitch_miner.mine(
     [
-       Streamer("CyberZombie__", settings=StreamerSettings(make_predictions=False  , follow_raid=True , claim_drops=True  , watch_streak=True , community_goals=False  , bet=BetSettings(strategy=Strategy.SMART      , percentage=20 , stealth_mode=True,  percentage_gap=4 , max_points=1234   , filter_condition=FilterCondition(by=OutcomeKeys.TOTAL_USERS,      where=Condition.LTE, value=4200 ) ) )),
+       Streamer("WikyamYT", settings=StreamerSettings(make_predictions=False  , follow_raid=True , claim_drops=True  , watch_streak=True , community_goals=False  , bet=BetSettings(strategy=Strategy.SMART      , percentage=20 , stealth_mode=True,  percentage_gap=4 , max_points=1234   , filter_condition=FilterCondition(by=OutcomeKeys.TOTAL_USERS,      where=Condition.LTE, value=4200 ) ) )),
        Streamer("Arguz11", settings=StreamerSettings(make_predictions=False , follow_raid=True  , claim_drops=True ,  watch_streak=True , community_goals=False  , bet=BetSettings(strategy=Strategy.PERCENTAGE , percentage=5 , stealth_mode=False, percentage_gap=20 , max_points=1234  , filter_condition=FilterCondition(by=OutcomeKeys.TOTAL_USERS,     where=Condition.GTE, value=1 ) ) )),
 
     ],                                  # Array of streamers (order = priority)
